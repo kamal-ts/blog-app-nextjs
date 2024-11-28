a52704c3e46c121ddc89c768077463460d4b56f5
kamal1234567890 <kamaluddin1623@gmail.com>
2024-11-29 02:02:50 +0900

    creating category list and adding responsive interface

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const CategoryList = () => {
   return (
-    <div>CategoryList</div>
-  )
-}
+    <div className="mt-12">
+      <h1 className="text-xl font-bold mb-6">Popular Categories </h1>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+        <ListCategory
+          href={"/blog"}
+          color={"bg-sky-100"}
+          image={"/style.png"}
+          title={"style"}
+        />
+        <ListCategory
+          href={"/blog"}
+          color={"bg-red-100"}
+          image={"/fashion.png"}
+          title={"fashion"}
+        />
+        <ListCategory
+          href={"/blog"}
+          color={"bg-yellow-100"}
+          image={"/food.png"}
+          title={"food"}
+        />
+        <ListCategory
+          href={"/blog"}
+          color={"bg-green-100"}
+          image={"/travel.png"}
+          title={"travel"}
+        />
+        <ListCategory
+          href={"/blog"}
+          color={"bg-purple-100"}
+          image={"/culture.png"}
+          title={"culture"}
+        />
+        <ListCategory
+          href={"/blog"}
+          color={"bg-pink-100"}
+          image={"/coding.png"}
+          title={"coding"}
+        />
+      </div>
+    </div>
+  );
+};
 
-export default CategoryList
+export default CategoryList;
+
+const ListCategory: React.FC<{
+  href: string;
+  color?: string;
+  title: string;
+  image: string;
+}> = ({ href, title, image }) => {
+  return (
+    <Link
+      href={href}
+      className={` btn`}
+    >
+      <div className="w-8 h-8 relative">
+        <Image
+          fill
+          src={image}
+          alt={title}
+          className="rounded-full object-cover border-2 border-white"
+        />
+      </div>
+      {title}
+    </Link>
+  );
+};
--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -4,12 +4,12 @@ import React from "react";
 const Featured = () => {
   return (
     <div>
-      <h1 className="text-7xl "><span className="font-bold">Hey, Mr.Malo here!</span> Discover my stories and creative ideas.</h1>
-      <div className="flex items-center gap-10 mt-12">
-        <div className="flex-1 h-72 relative">
+      <h1 className="text-5xl lg:text-7xl "><span className="font-bold">Hey, Mr.Malo here!</span> Discover my stories and creative ideas.</h1>
+      <div className="md:flex items-center gap-10 mt-12 hidden">
+        <div className="flex-1 h-80 relative ">
           <Image src={"/p1.jpeg"} fill alt="" className="object-cover rounded-xl" />
         </div>
-        <div className="flex-1 h-72 flex items-center">
+        <div className="flex-1 h-80 flex items-center">
           <div>
             <h1 className="text-2xl font-bold">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis, aspernatur?</h1>
             <p className="py-6">
